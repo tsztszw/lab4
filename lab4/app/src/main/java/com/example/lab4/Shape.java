32a67776f9a230e26829aa50e4c537bf726696fc
Xu Dai <xdai2@cs.ualberta.ca>
2019-10-01 19:40:45 -0600

    add

--- lab4/app/src/main/java/com/example/lab4/Shape.java
+++ lab4/app/src/main/java/com/example/lab4/Shape.java
@@ -3,8 +3,13 @@ package com.example.lab4;
 public abstract class Shape {
     int x;
     int y;
+    String color = "red";
     public Shape(int x, int y){
         this.x = x;
         this.y = y;
+    }
+
+    public Shape(){
+
     }
 }
---
+++ lab4/app/src/main/java/com/example/lab4/Star.java
@@ -0,0 +1,7 @@
+package com.example.lab4;
+
+public  class Star extends Shape{
+
+
+
+}
